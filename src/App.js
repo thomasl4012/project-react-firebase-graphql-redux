1264afb3c3e2798b7f1ec4bbc4cb7ddda599d0d4
Thomas Lamy <thomaslamy75011@gmail.com>
2021-03-08 01:16:24 +0100

    add sass on homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Homepage from "./homepage.component";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Homepage></Homepage>
+    </div>
+  );
 }
 
 export default App;
